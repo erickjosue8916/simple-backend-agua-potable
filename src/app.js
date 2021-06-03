71243b0b452f7721baa5a66367fc868ff361ab10
Erick Saravia <ericks@labs.aeegle.com>
2021-06-03 14:09:50 -0600

    fix: adding cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const {config} = require('./config')
 const dotenv = require('dotenv')
 const {api} = require('./api')
@@ -6,6 +7,7 @@ const {api} = require('./api')
 dotenv.config()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', api)
